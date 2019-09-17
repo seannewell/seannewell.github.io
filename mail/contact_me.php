89ad4cd56c047ffbdbdb5293d5fec8a76918413e
snewell0923 <snewell0923@att.net>
2019-09-16 19:54:40 -0500

    changed picture, typos, contact section

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,5 +1,5 @@
 <?php
-// Check for empty fields
+Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
